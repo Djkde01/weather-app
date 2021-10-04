1fdad1485249d7e8619536c6e926d82b31b3b99b
Djkde <sergio.estrella@utp.edu.co>
2021-10-04 12:45:08 -0500

    [FIX] Fixed app crash when results were not found

--- src/app.js
+++ src/app.js
@@ -34,21 +34,24 @@ app.get("/weather", (req, res) => {
   if (!req.query.address) {
     return res.send({ error: "You must provide an address" });
   }
-  geoCode(req.query.address, (error, { latitude, longitude, location }) => {
-    if (error) {
-      return res.send({ error: error });
-    }
-    forecast(latitude, longitude, (error, forecastData) => {
+  geoCode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error: error });
       }
-      res.send({
-        address: req.query.address,
-        location: location,
-        forecast: forecastData,
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error: error });
+        }
+        res.send({
+          address: req.query.address,
+          location: location,
+          forecast: forecastData,
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/help/*", (req, res) => {
