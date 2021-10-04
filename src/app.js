f51d1d24c4d9f0e97b640e83ad38023e87a999b8
Djkde <sergio.estrella@utp.edu.co>
2021-10-04 12:24:02 -0500

    [NEW] Added geoCode & forecast utils & connected with  weater route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
+const geoCode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
@@ -29,12 +31,23 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About the page" });
 });
 app.get("/weather", (req, res) => {
-  res.send({
-    location: {
-      latitude: 54,
-      longitude: -15,
-    },
-    forecast: "Rainy",
+  if (!req.query.address) {
+    return res.send({ error: "You must provide an address" });
+  }
+  geoCode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error: error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error: error });
+      }
+      res.send({
+        address: req.query.address,
+        location: location,
+        forecast: forecastData,
+      });
+    });
   });
 });
 
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("postman-request");
+
+const forecast = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=9b1d896407e025571cd632a18b7bcce5&query=${lat},${long}&units=m`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect, please try again", undefined);
+    } else if (body.error) {
+      callback(body.error.info, undefined);
+    } else {
+      const temperature = body.current.temperature;
+      const feelsLike = body.current.feelslike;
+      const forecast = body.current.weather_descriptions[0];
+      callback(
+        undefined,
+        `${forecast}. It is currently ${temperature}° out. It feels like ${feelsLike}° out`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
