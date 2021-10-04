6527ea79f6e5ce95e399efd2dd35dab005c38aab
Djkde <sergio.estrella@utp.edu.co>
2021-10-04 13:48:38 -0500

    [NEW] Connected forecast endpoint to web app

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-console.log("This shit is running");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(
+    `http://localhost:3000/weather?address=${encodeURIComponent(search.value)}`
+  ).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data);
+      }
+    });
+  });
+});
